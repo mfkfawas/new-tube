576fc81f43319e4cdb13ddb2fe771191c27370c6
mfkfawas <mfkfawas@gmail.com>
2025-02-28 15:18:40 +0300

    setup(ui): logo added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-  <Button>Hello World</Button>
+    <>
+    <Image src='/logo.svg' height={50} width={50} alt="logo" />
+    <Button>Hello World</Button>
+    </>
   );
 }
