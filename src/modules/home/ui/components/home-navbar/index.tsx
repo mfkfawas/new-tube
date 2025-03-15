ff5f44e51d1195b2a9769dc2fec7d9ada21f7a31
mfkfawas <mfkfawas@gmail.com>
2025-03-16 01:32:27 +0300

    feat(ui): home nav bar

---
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { SidebarTrigger } from "@/components/ui/sidebar"
+
+import { SearchInput } from "./search-input"
+
+export function HomeNavBar() {
+  return (
+    <nav className="fixed top-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50 w-full">
+      <div className="flex items-center gap-4 w-full">
+        {/* Menu and Logo */}
+        <div className="flex items-center flex-shrink-0">
+          <SidebarTrigger />
+          <Link href="/">
+            <div className="p-4 flex items-center gap-1">
+              <Image src="/logo.svg" alt="Logo" width={32} height={32} />
+              <p className="text-xl font-semibold tracking-tight">NewTube</p>
+            </div>
+          </Link>
+        </div>
+
+        {/* Search Bar */}
+        <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
+          <SearchInput />
+        </div>
+      </div>
+    </nav>
+  )
+}
