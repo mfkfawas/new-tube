cda31f00d358f6e59ae457512346db4487c90228
mfkfawas <mfkfawas@gmail.com>
2025-04-23 22:34:49 +0300

    feat(ui): refactor HomeNavBar and add HomeSidebar component

--- src/modules/home/ui/components/home-navbar/index.tsx
+++ src/modules/home/ui/components/home-navbar/index.tsx
@@ -8,27 +8,25 @@ import { AuthButton } from "@/modules/auth/ui/components/auth-button"
 
 export function HomeNavBar() {
   return (
-    <nav className="fixed top-0 right-0 h-16 bg-white flex items-center px-2 pr-5 z-50 w-full">
-      <div className="flex items-center gap-4 w-full">
-        {/* Menu and Logo */}
-        <div className="flex items-center flex-shrink-0">
-          <SidebarTrigger />
-          <Link href="/">
-            <div className="p-4 flex items-center gap-1">
-              <Image src="/logo.svg" alt="Logo" width={32} height={32} />
-              <p className="text-xl font-semibold tracking-tight">NewTube</p>
-            </div>
-          </Link>
-        </div>
+    <nav className="fixed top-0 start-0 end-0 h-16 bg-white flex items-center gap-4 px-2 pr-5 z-50">
+      {/* Menu and Logo */}
+      <div className="flex items-center flex-shrink-0">
+        <SidebarTrigger />
+        <Link href="/">
+          <div className="p-4 flex items-center gap-1">
+            <Image src="/logo.svg" alt="Logo" width={32} height={32} />
+            <p className="text-xl font-semibold tracking-tight">NewTube</p>
+          </div>
+        </Link>
+      </div>
 
-        {/* Search Bar */}
-        <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
-          <SearchInput />
-        </div>
+      {/* Search Bar */}
+      <div className="flex-1 flex justify-center max-w-[720px] mx-auto">
+        <SearchInput />
+      </div>
 
-        <div className="flex-shrink-0 items-center flex gap-4">
-          <AuthButton />
-        </div>
+      <div className="flex-shrink-0 items-center flex gap-4">
+        <AuthButton />
       </div>
     </nav>
   )
--- src/modules/home/ui/layouts/home-layout.tsx
+++ src/modules/home/ui/layouts/home-layout.tsx
@@ -1,14 +1,16 @@
-import { PropsWithChildren } from "react";
-import { SidebarProvider } from "@/components/ui/sidebar";
-import { HomeNavBar } from "@/modules/home/ui/components/home-navbar";
+import { PropsWithChildren } from "react"
+import { SidebarProvider } from "@/components/ui/sidebar"
+import { HomeNavBar } from "@/modules/home/ui/components/home-navbar"
+import { HomeSidebar } from "@/modules/home/ui/components/home-sidebar"
 
 export function HomeLayout({ children }: PropsWithChildren) {
   return (
     <SidebarProvider>
       <div className="w-full">
         <HomeNavBar />
-        <div>
-          {children}
+        <div className="flex min-h-screen pt-16">
+          <HomeSidebar />
+          <main className="flex-1 overflow-y-auto">{children}</main>
         </div>
       </div>
     </SidebarProvider>
