ce0cb012b6810835c4940b5d68f8dd2d9e10c5c1
mfkfawas <mfkfawas@gmail.com>
2025-04-25 21:00:41 +0300

    feat(auth): integrate Clerk for authentication with sign-in and sign-up pages
    - Added @clerk/nextjs dependency for authentication.
    - Created a layout component for authentication pages.
    - Implemented catch-all routes for sign-in and sign-up pages using Clerk's SignIn and SignUp components.
    - Updated the root layout to include ClerkProvider for authentication context.
    - Added middleware for Clerk to handle authentication routes.

---
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,6 @@
+// [[...sign-in]] - is an example of catch-all route
+import { SignIn } from "@clerk/nextjs"
+
+export default function Page() {
+  return <SignIn />
+}
---
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -0,0 +1,6 @@
+// [[...sign-up]] - is an example of catch-all route
+import { SignUp } from "@clerk/nextjs"
+
+export default function Page() {
+  return <SignUp />
+}
