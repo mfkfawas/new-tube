f8ca99e36cdefa5f290a36ac9627a431332e8114
mfkfawas <mfkfawas@gmail.com>
2025-05-30 09:02:28 +0300

    docs(trpc): add comments to clarify TRPC usage and data loading concepts

--- src/app/(home)/client.tsx
+++ src/app/(home)/client.tsx
@@ -3,6 +3,13 @@
 import { trpc } from "@/trpc/client"
 
 export function PageClient() {
+  // NOTE: In this new Antonio's trpc pattern every useSuspenseQuery should have following prefetch in corr page.tsx with same param, eg:({ text: "fawas" }) passed
+  // we are using the server component(page.tsx) as "loaders"
+  // "render as you fetch" concept
+  // refer the screenshot in this directory
+  // faster load time
+  // parallel data loading
+  // const [data] = trpc.hello.useSuspenseQuery({ text: "fawas" })
   const [data] = trpc.hello.useSuspenseQuery({ text: "fawas" })
   return <div>page client says {data.greeting}</div>
 }
