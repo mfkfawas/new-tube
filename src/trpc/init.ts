4b8abda16051f18db31046f1dea929401be5cee0
mfkfawas <mfkfawas@gmail.com>
2025-05-30 09:25:32 +0300

    feat(trpc): enhance TRPC context to pass clerkUserId

--- src/trpc/init.ts
+++ src/trpc/init.ts
@@ -1,18 +1,25 @@
+import { auth } from "@clerk/nextjs/server"
 import { initTRPC } from "@trpc/server"
 import { cache } from "react"
 import superjson from "superjson"
 
+// NOTE: This context would be available for every single API call that you do, so we need to keep it light as possible
 export const createTRPCContext = cache(async () => {
   /**
    * @see: https://trpc.io/docs/server/context
    */
-  return { userId: "user_123" }
+
+  // NOTE: This auth() method will not make any API call instead destructure, so this would be lightweight.
+  const { userId } = await auth()
+  return { clerkUserId: userId }
 })
+export type Context = Awaited<ReturnType<typeof createTRPCContext>>
+
 // Avoid exporting the entire t-object
 // since it's not very descriptive.
 // For instance, the use of a t variable
 // is common in i18n libraries.
-const t = initTRPC.create({
+const t = initTRPC.context<Context>().create({
   /**
    * @see https://trpc.io/docs/server/data-transformers
    */
