b40e85d4caaf761e3ba6998c8052e906b112b227
mfkfawas <mfkfawas@gmail.com>
2025-06-05 05:12:03 +0300

    fix(trpc): fix undefined issue while using useSuspenseQuery for its corresponding void prefetch

--- src/trpc/query-client.ts
+++ src/trpc/query-client.ts
@@ -9,9 +9,7 @@ export function makeQueryClient() {
       },
       dehydrate: {
         // serializeData: superjson.serialize,
-        shouldDehydrateQuery: (query) =>
-          defaultShouldDehydrateQuery(query) ||
-          query.state.status === "pending",
+        shouldDehydrateQuery: (query) => defaultShouldDehydrateQuery(query),
       },
       hydrate: {
         deserializeData: superjson.deserialize,
