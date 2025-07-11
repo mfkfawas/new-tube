4dbbb8677cbb425a3fd91fb2cc5e5da90b0a9921
mfkfawas <mfkfawas@gmail.com>
2025-07-11 21:50:26 +0300

    feat(studio): implement video retrieval with infinite scrolling and create VideosSection component

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -1,3 +1,22 @@
-export default function Page() {
-  return <div>Studio</div>
+import { HydrateClient, trpc } from "@/trpc/server"
+import { StudioView } from "@/modules/studio/ui/views/studio-view"
+import { DEFAULT_LIMIT } from "@/constants"
+
+export default async function Page() {
+  //NOTE: If you are using prefetchInfinite() then you have to use useSuspenseInfiniteQuery() in the corr client component where you are trying to use this prefetched data
+  void trpc.studio.getMany.prefetchInfinite(
+    {
+      limit: DEFAULT_LIMIT,
+    },
+    {
+      pages: 1, // Prefetch only the first page initially
+      getNextPageParam: (lastPage) => lastPage.nextCursor,
+    }
+  )
+
+  return (
+    <HydrateClient>
+      <StudioView />
+    </HydrateClient>
+  )
 }
